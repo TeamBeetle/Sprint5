c7e45b5ae67a4c3765aabe12ca57d932b82a3e54
EverettHanke <forgetekbusiness@gmail.com>
2024-01-29 22:44:09 -0800

    Add Application Additions

---
+++ vardump.php
@@ -0,0 +1,32 @@
+
+<!-- APPLICATION ANNOUNCEMENT PHP -->
+<?php
+error_reporting(0);
+
+//to will later redirect to Tyler Email.
+$to = "Everett Hanke <hanke.everett@student.greenriver.edu>,";
+$subject = $_POST['app-position'] . " : ". $_POST['app-employer'];
+$message = "NEW OPPROTUNITIES FOUND AT " . $_POST['app-employer'] . " Looking for a " . $_POST['app-position'] . " This is a " . $_POST['app-status'] . " position. " . "\n"
+ . $_POST['app-info'] . " Apply here : " . $_POST['app-link'] . " , " . $_POST['app-date'];
+
+$headers = 'From: ' . $subject . "\r\n" .
+    'Reply-To : ' . $subject . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+if (mail($to, $subject,$message, $headers))
+{
+    echo "<h> Message Sent Successfully </h>";
+    echo "<p> " . $message . " </p>";
+}
+else
+{
+    echo "<h> Message Failed to Deliver </h>";
+}
+
+
+?>
+
+<!--  -->
+<?php
+
+?>
