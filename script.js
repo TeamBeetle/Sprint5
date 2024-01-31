ef5a498c1873b4b552e05ced5f4d4282115fa121
EBoydGRC <EBoydGRC@noreply.github.com>
2024-01-30 16:23:43 -0800

    Cleared some PHP Storm warnings.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ toggleContainer.addEventListener('click', function(e)
 
   //preform actions.
   //Note: page.attributes[1].value refers to data-bs-theme in <html>
-  if(page.attributes[1].value == 'light')
+  if(page.attributes[1].value === 'light')
   {
     page.attributes[1].value = 'dark';
     document.getElementById("main-logo").src="images/TeamBeetleLogoDarkMode.png";
@@ -114,7 +114,7 @@ function validateName(inputName)
 }
 function validateEmail(inputEmail)
 {
-  if(inputEmail.match(/(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i))
+  if(inputEmail.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])/i))
   {
     document.querySelector('#email-error').style.display = "none";
     return 0;
