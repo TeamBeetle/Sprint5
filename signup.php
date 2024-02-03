d0ba3e329e4027fd7f1e640030dcfad170d88535
gballreich <garrett.ballreich101@gmail.com>
2024-02-03 14:58:11 -0800

    worked on form validation for signup.php, updated the action ="" for contact and sign up form in index.html

--- signup.php
+++ signup.php
@@ -1,64 +1,162 @@
 <?php
 error_reporting(0);
-$name = $email = $cohort ="";
-$nameErr = $emailErr = $cohortErr = $boxErr = "";
-$seeking= $seekingErr = "";
-$interests= $interestsErr = "";
 
+$userName=$userEmail=$userCohort="";
+$internship=$job=$searching="";
 
-//checks name
+$nameCheck = "";
+$emailCheck="";
+$cohortCheck="";
+
+$seekingInternship=$seekingJob=$notSearching="";
+$InterncheckBoxCount=$jobCheckBoxCount=$searchingCheckBoxCount="";
+
+$checkboxSum="";
+$userInerest=$userInerestCheck="";
+
+//validates name
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["user-name"])) {
-        $nameErr = "Name is required";
-//once "user-name" is not null and saved into $name variable
-//$name is checked again through preconfigured match and validates no special characters
-    } elseif (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-        $nameErr = "Only letters and white space allowed";
-        $name = "";
-    } //if no special characters are found "user-name" value is stored in $name
-    else {
-        $name = $_POST["user-name"];
-    }
-//checks email
-    if (empty($_POST["user-email"])){
-        $emailErr = "Email is required";
-//uses php filter_var method to check the value of "user-email" if it fails a new error message is stored
-//in $toErr for improper email format.
-    }elseif(!filter_var($_POST["user-email"], FILTER_VALIDATE_EMAIL)){
-        $emailErr = "Invalid email format";
-    }else{
-        $email = $_POST["user-email"];
-    }
-    //checks cohort
-    if(empty($_POST['user-cohort'])){
-        $cohortErr = "Cohort is required";
-    }elseif($_POST['user-cohort']<1 || $_POST['user-cohort']>100){
-        $cohortErr = "Please enter a value between 1 - 100";
-    }elseif(is_numeric(["user-cohort"])){
-        $cohortErr = "Please enter a number";
-    }else{
-        $cohort = $_POST['user-cohort'];
-    }
-//checks for at least 1 checkbox to be clicked
-    if (isset($_POST['submit'])) {
-        if(empty($_POST['seeking']))
-        {
-            $seekingErr = "You must select 1 checkbox";
-        }else{
-            $seeking = $_POST['seeking'];
-        }
-    }
-//checks for field of interest
-    if(empty(["field-interest"])){
-        $interestsErr = "Please enter at least 1 field of interest";
-    }else{
-        $interests = $_POST["field-interests"];
-    }
-}
-?>
-<html lang="en">
-<body>
-thank you for signing up .<?php echo '$name' ?>.
-</body>
-</html>
+    if (empty($_POST['user-name'])){
+        $nameCheck = 0;
 
+    } else {
+        $nameCheck = 1;
+        $userName = $_POST['user-name'];
+
+    }
+//validates email
+    if(empty($_POST['user-email'])){
+        $emailCheck = 0;
+    }else{
+        $emailCheck = 1;
+        $userEmail = $_POST['user-email'];
+    }
+//validates cohort
+    if(empty($_POST['user-cohort'])){
+        $cohortCheck = 0;
+    }else{
+        $cohortCheck = 1;
+        $userCohort = $_POST['user-cohort'];
+    }
+//validate internship checkbox
+    if(empty($_POST['internship'])){
+        $$InterncheckBoxCount = 0;
+    }else{
+        $seekingInternship="Seeking an Internship";
+        $InterncheckBoxCount = 1;
+    }
+//validate job checkbox
+    if(empty($_POST['job'])){
+        $jobCheckBoxCount = 0;
+    }else{
+        $seekingJob="Seeking a Job";
+        $jobCheckBoxCount = 1;
+    }
+//validate not searching checkbox
+    if(empty($_POST['searching'])){
+        $searchingCheckBoxCount=0;
+
+    }else{
+        $notSearching = "Not Actively Searching";
+        $searchingCheckBoxCount = 1;
+
+    }
+//validate fields of interest
+    if(empty($_POST['field-interests'])){
+        $userInerestCheck = 0;
+    }else{
+        $userInerestCheck = 1;
+        $userInerest = $_POST['field-interests'];
+    }
+
+}
+$checkboxSum = ($InterncheckBoxCount + $jobCheckBoxCount + $searchingCheckBoxCount);
+$userNameCap = ucfirst("$userName");
+
+//displays "incomplete" form page
+if($nameCheck == 0 || $emailCheck == 0 || $cohortCheck == 0 || $checkboxSum < 1){
+    echo"
+ <html lang='en'>
+    <head>
+    <link href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity = 'sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN'crossorigin='anonymous'>
+    <link href='style.css' rel='stylesheet' type='text/css'/>
+    </head>
+    <body>
+    <nav id='background' class='navbar navbar-expand-md  navbar-dark'>
+
+  <img id='grc-logo' class='navbar-brand'  src='images/GRC Logo.png'>
+  <button class='navbar-toggler' type='button' data-toggle='collapse'
+          data-target='#collapsibleNavbar'>
+    <span class='navbar-toggler-icon'></span>
+  </button>
+    <div class='collapse navbar-collapse links' id='collapsibleNavbar'>
+
+    <ul class='navbar-nav links'>
+      <li class='nav-item'>
+        <a class='nav-link' href='#'>Dashboard</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link sign-up' href='#'>Sign Up</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link add-application' href='#'>Add Applicaion</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link contact-anchor contact' href='#'>Contact</a>
+      </li>
+    </ul>
+  </div>
+  <div id='toggleContainer' class='col-2'>
+    <button type='button' class='btn toggle active-mode btn-light'>Light</button>
+    <button type='button' class='btn toggle btn-dark'>Dark</button>
+  </div>
+</nav>
+    <p>Please enter your name, email address, cohort number, and at least 1 checkbox must be selected.</p>
+    </body>
+    </html>
+    ";
+}else{
+//displays "complete" form page
+    echo"
+    <html lang='en'>
+    <head>
+    <link href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity = 'sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN'crossorigin='anonymous'>
+    <link href='style.css' rel='stylesheet' type='text/css'/>
+    </head>
+    <body>
+    <nav id='background' class='navbar navbar-expand-md  navbar-dark'>
+
+  <img id='grc-logo' class='navbar-brand'  src='images/GRC Logo.png'>
+  <button class='navbar-toggler' type='button' data-toggle='collapse'
+          data-target='#collapsibleNavbar'>
+    <span class='navbar-toggler-icon'></span>
+  </button>
+    
+  <div class='collapse navbar-collapse links' id='collapsibleNavbar'>
+
+    <ul class='navbar-nav links'>
+      <li class='nav-item'>
+        <a class='nav-link' href='#'>Dashboard</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link sign-up' href='#'>Sign Up</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link add-application' href='#'>Add Applicaion</a>
+      </li>
+      <li class='nav-item'>
+        <a class='nav-link contact-anchor contact' href='#'>Contact</a>
+      </li>
+    </ul>
+  </div>
+  <div id='toggleContainer' class='col-2'>
+    <button type='button' class='btn toggle active-mode btn-light'>Light</button>
+    <button type='button' class='btn toggle btn-dark'>Dark</button>
+  </div>
+</nav>  
+ <p>Thank you for signing up $userNameCap, we will email you at $userEmail. 
+    Welcome to Cohort $userCohort!</p>
+</body>
+</html> 
+    ";
+}
