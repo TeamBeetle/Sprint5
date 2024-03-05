99f250f80b5442a7c03d1bcc3f9cb10f18d16e0a
EBoydGRC <EBoydGRC@noreply.github.com>
2024-03-05 12:42:27 -0800

    Fixed GRC Icon link

--- admin-page.php
+++ admin-page.php
@@ -14,7 +14,7 @@
 <!-- Nav Bar -->
 <nav class="navbar navbar-expand-lg navbar-dark navigation">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">
+        <a class="navbar-brand" href="index.php">
             <img id="grc-logo" src="images/GRC%20Logo.png" alt="Green River College Logo">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -420,6 +420,6 @@
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-<script src="admin.js?v=20"></script>
+<script src="admin.js?v=21"></script>
 </body>
 </html>
